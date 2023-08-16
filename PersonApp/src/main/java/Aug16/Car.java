662c50ed3f90f91dc7aaaf77642b3d138a8b4711
Zain Mohammed <zain.remo@gmail.com>
2023-08-16 17:54:03 -0500

    CarType encapsulation by Zain for Aug16 Assignment

--- PersonApp/src/main/java/Aug16/Car.java
+++ PersonApp/src/main/java/Aug16/Car.java
@@ -4,11 +4,16 @@ public class Car extends Vehicle {
     public Car() {
         super("Vehicle");
     }
+    public void drive() {
+        System.out.println("The car is being driven");
+    }
 
     /*
     Commenting out this as i have used the default constructor for calling the vehicle class
     public Car(String type){
         super("Vehicle");
     }
+
+
     */
 }
--- PersonApp/src/main/java/Aug16/Main.java
+++ PersonApp/src/main/java/Aug16/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Car ob = new Car();
         ob.start();
+        ob.drive();
     }
 }
